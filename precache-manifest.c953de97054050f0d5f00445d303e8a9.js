866e6a195417f3d8a16d6491285538710dd4a828
Zhe Chang <tedchang@zhes-mbp.devices.brown.edu>
2019-09-09 22:43:24 -0400

    Updates

---
+++ precache-manifest.c953de97054050f0d5f00445d303e8a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca02e89ccb2c09d0c91a9928a856b3a7",
+    "url": "/tchang-portfolio/index.html"
+  },
+  {
+    "revision": "6721c8874c95bada26be",
+    "url": "/tchang-portfolio/static/css/main.a6b128fe.chunk.css"
+  },
+  {
+    "revision": "40b0ebc9a1887099d0ee",
+    "url": "/tchang-portfolio/static/js/2.18fd3e52.chunk.js"
+  },
+  {
+    "revision": "6721c8874c95bada26be",
+    "url": "/tchang-portfolio/static/js/main.f9bbdc20.chunk.js"
+  },
+  {
+    "revision": "2c2957666f9d2891e8d4",
+    "url": "/tchang-portfolio/static/js/runtime~main.d57cff9a.js"
+  },
+  {
+    "revision": "94d99b4b44e8b5360f4024ade88d5a61",
+    "url": "/tchang-portfolio/static/media/MyResume.94d99b4b.pdf"
+  },
+  {
+    "revision": "55a6b822d4ccfa67d4c73d4aa5f69c62",
+    "url": "/tchang-portfolio/static/media/code.55a6b822.svg"
+  },
+  {
+    "revision": "74cff3e93660521c2a5f6f52ab431a5c",
+    "url": "/tchang-portfolio/static/media/eye.74cff3e9.svg"
+  },
+  {
+    "revision": "48fd6a1372e051daeee1da7c8a1bff5b",
+    "url": "/tchang-portfolio/static/media/facebook.48fd6a13.svg"
+  },
+  {
+    "revision": "0afffe7d0343b9a607a014dfaddeaed4",
+    "url": "/tchang-portfolio/static/media/github.0afffe7d.svg"
+  },
+  {
+    "revision": "a42fecfa2a74ac42e927fd170c724646",
+    "url": "/tchang-portfolio/static/media/linkedin.a42fecfa.svg"
+  },
+  {
+    "revision": "098a7c5fa730250c16f900976e40fdcc",
+    "url": "/tchang-portfolio/static/media/me.098a7c5f.jpeg"
+  }
+]);
